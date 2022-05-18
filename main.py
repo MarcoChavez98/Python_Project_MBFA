c21766eaafcdb54d168038ef0d84f67f0db902b0
MarcoChavez98 <105824935+MarcoChavez98@users.noreply.github.com>
2022-05-19 01:02:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 header = st.container()
 texte = st.container()
 with header:
-    st.title("HELLO LES ENFANTS!!!!")
+    st.title("HELLO LES ENFANTS!!!! :pizza:")
 
 with texte:
     st.subheader("Cela ressemble à ça d'ecrire avec streamlit")
