046e3747d2769f4696d3fa08b7151abdea37ce0c
MarcoChavez98 <105824935+MarcoChavez98@users.noreply.github.com>
2022-05-19 01:21:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ with texte:
     st.subheader("Cela ressemble à ça d'écrire avec streamlit")
 
     st.write("C'est là où toute la magie va se passer")
+    
+cool = st.slider("C'est pas cool?",0,100)
